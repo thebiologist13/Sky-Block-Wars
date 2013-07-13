40eb88fa11ee73956ce09ea64a64fe357279998f
riking <rikingcoding@gmail.com>
2013-07-12 23:06:12 -0700

    Make InvManager use ItemStack Lists instead of base64
    Removes a OBC usage

--- src/me/kyle/burnett/SkyBlockWarriors/InvManager.java
+++ src/me/kyle/burnett/SkyBlockWarriors/InvManager.java
@@ -1,42 +1,59 @@
 package me.kyle.burnett.SkyBlockWarriors;
 
-import me.kyle.burnett.SkyBlockWarriors.Configs.ConfigManager;
-import me.kyle.burnett.SkyBlockWarriors.Utils.InventoryUtil;
+import java.util.Arrays;
+import java.util.List;
 
+import me.kyle.burnett.SkyBlockWarriors.Configs.ConfigManager;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 public class InvManager {
-	
-	static InvManager instance = new InvManager();
-	
-	public static InvManager getInstance(){
-		return instance;
-	}
-	
-	public void saveInv(Player p){
-		
-		Inventory main = InventoryUtil.getInstance().getContentInventory(p.getInventory());
-		Inventory armor = InventoryUtil.getInstance().getArmorInventory(p.getInventory());
-		
-		Main.getInstance().Inv.set(p.getName() +  ".Main", InventoryUtil.getInstance().toBase64(main));
-		Main.getInstance().Inv.set(p.getName() + ".Armor", InventoryUtil.getInstance().toBase64(armor));
-		
-		p.getInventory().clear();
-		p.getInventory().setArmorContents(null);
-		
-		ConfigManager.getInstance().saveYamls();
-	}
-	
-	public void restoreInv(Player p){
-		
-		Inventory inv = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Main"));
-		Inventory armor = InventoryUtil.getInstance().fromBase64(Main.getInstance().Inv.getString(p.getName() + ".Armor"));
 
-		p.getInventory().clear();
-		p.getInventory().setContents(inv.getContents());
-		p.getInventory().setArmorContents(armor.getContents());
-		
-	}
+    static InvManager instance = new InvManager();
 
+    public static InvManager getInstance() {
+        return instance;
+    }
+
+    public void saveInv(Player p) {
+        List<ItemStack> main = Arrays.asList(p.getInventory().getContents());
+        List<ItemStack> armor = Arrays.asList(p.getInventory().getArmorContents());
+
+        Main.getInstance().Inv.set(p.getName() + ".Main", main);
+        Main.getInstance().Inv.set(p.getName() + ".Armor", armor);
+
+        p.getInventory().clear();
+        p.getInventory().setArmorContents(null);
+
+        ConfigManager.getInstance().saveYamls();
+    }
+
+    @SuppressWarnings("unchecked")
+    public void restoreInv(Player p) {
+        List<ItemStack> main = (List<ItemStack>) Main.getInstance().Inv.getList(p.getName() + ".Main");
+        List<ItemStack> armor = (List<ItemStack>) Main.getInstance().Inv.getList(p.getName() + ".Armor");
+
+        // Check the cast
+        try {
+            for (@SuppressWarnings("unused")
+            ItemStack i : main)
+                ;
+            for (@SuppressWarnings("unused")
+            ItemStack i : armor)
+                ;
+        } catch (ClassCastException e) {
+            p.sendMessage(ChatColor.RED + "There was an error restoring your inventory! :(");
+            p.sendMessage(ChatColor.RED + "The admin has been notified.");
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Error: Unable to restore inventory of " + ChatColor.YELLOW + p.getName());
+            Bukkit.getConsoleSender().sendMessage(ChatColor.RED + "Please inspect the inventorys.yml file.");
+            e.printStackTrace();
+        }
+
+        p.getInventory().setContents(main.toArray(new ItemStack[36]));
+        p.getInventory().setArmorContents(armor.toArray(new ItemStack[4]));
+    }
 }
